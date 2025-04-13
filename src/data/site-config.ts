3563ade1ab9595ec89c06cb5c18a1ed18af61e75
Mysky <kgruth@hotmail.com>
2025-04-12 22:38:53 -0500

    update site info

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -44,7 +44,7 @@ const siteConfig: SiteConfig = {
     subtitle: 'Mi personal blog',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
-        src: '/public/dante-preview.jpg',
+        src: 'dante-preview.jpg',
         alt: 'Dante - Astro.js and Tailwind CSS theme'
     },
     headerNavLinks: [
