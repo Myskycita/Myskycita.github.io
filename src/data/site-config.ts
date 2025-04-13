23c5abf41b229b7d8ca5741f0d397229f64df389
Mysky <kgruth@hotmail.com>
2025-04-12 22:43:20 -0500

    git add . git commit -m "Actualizo imagen a nuevo-banner.jpg" git push origin main

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -45,7 +45,7 @@ const siteConfig: SiteConfig = {
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
         src: 'dante-preview.jpg',
-        alt: 'Dante - Astro.js and Tailwind CSS theme'
+        alt: 'nueva imagen para el sitio web'
     },
     headerNavLinks: [
         {
